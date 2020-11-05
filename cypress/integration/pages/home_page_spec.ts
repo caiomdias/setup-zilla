d80e39b707f7d7e0e57bf440cb6f24bc92bda96b
Caio M. Dias <kaiomt1@gmail.com>
2020-11-05 16:08:28 -0300

    feat: update cypress

--- cypress/integration/pages/home_page_spec.ts
+++ cypress/integration/pages/home_page_spec.ts
@@ -2,10 +2,6 @@ describe('The Home Page', () => {
   it('successfully loads', () => {
     cy.visit('/');
   });
-
-  it('Check the zilla home image', () => {
-    expect(true).toBeTruthy();
-  });
 });
 
 export {};
