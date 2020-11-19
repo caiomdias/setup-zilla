e4f2e93b9f1f545d1dd75e9b0990968d4e0e74d6
Caio M. Dias <kaiomt1@gmail.com>
2020-11-19 05:36:47 -0300

    chore: resolve build error

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -3,4 +3,6 @@ import React from 'react';
 // Styles
 import { Container } from '../../styles/components/layout';
 
-export const Layout: React.FC = ({ children }) => <Container data-testid="layout-container">{children}</Container>;
+const Layout: React.FC = ({ children }) => <Container data-testid="layout-container">{children}</Container>;
+
+export default Layout;
