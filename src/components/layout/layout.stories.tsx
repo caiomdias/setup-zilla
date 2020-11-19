ddcbf94fd8e8af8a2139b74152a8ee624a34d8e0
Caio M. Dias <kaiomt1@gmail.com>
2020-11-19 05:29:22 -0300

    feat: add storybook

---
+++ src/components/layout/layout.stories.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+
+import { ThemeProvider } from 'styled-components';
+
+import { Layout } from './layout';
+import theme from '../../styles/themes/example';
+
+export default {
+  title: 'DefaultLayout'
+}
+
+export const DefaultLayout: React.FC<{}> = () => (
+<ThemeProvider theme={theme}>
+  <Layout> My Layout </Layout>
+</ThemeProvider>
+)
--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -3,6 +3,4 @@ import React from 'react';
 // Styles
 import { Container } from '../../styles/components/layout';
 
-const Layout: React.FC = ({ children }) => <Container data-testid="layout-container">{children}</Container>;
-
-export default Layout;
+export const Layout: React.FC = ({ children }) => <Container data-testid="layout-container">{children}</Container>;
